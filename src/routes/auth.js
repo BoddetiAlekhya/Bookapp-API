72f3f3a0ef4cfbaebced6fb1914d3b7ef0e8030b
BoddetiAlekhya <boddeti.alekhya@gmail.com>
2019-02-25 21:01:38 +0530

    credentials validation

---
+++ src/routes/auth.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import User from '../models/User'
+
+const router = express.Router();
+
+router.post("/", (req,res) => {
+    const {credentials} = req.body;
+    console.log(credentials);
+    
+    User.findOne({email: credentials.email}).then(user => {
+        console.log("user",user);
+        
+        if(user && user.isValidPassword(credentials.password)){
+            res.json({user: {email: user.email}})
+        }else{
+            res.status(400).json({errors: {global: "Invali Credentials"}})
+        }
+    })
+})
+
+export default router;
